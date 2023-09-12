3e62b0a86eab78f0fe0dcc9c9f9a94b5d2d08905
jainishpatel28 <100318747+jainishpatel28@users.noreply.github.com>
2023-09-11 20:58:15 -0600

    prime-number-or-not.py

--- prime-number-or-not.py
+++ prime-number-or-not.py
@@ -1,12 +1,4 @@
-#Write your code below this line 👇
 def prime_checker(number):
-    # if number % 2 == 0 or number % 3 == 0 or number % 5 == 0 or number % 7 == 0:
-    #     if number == 2 or number == 3 or number == 5 or number == 7:
-    #         print("It's a prime number.")
-    #     else:
-    #         print("It's not a prime number.")
-    # else:
-    #     print("It's a prime number.")
     is_prime = True
     for a in range(2, number):
         if number % a == 0:
@@ -16,8 +8,6 @@ def prime_checker(number):
     else:
         print("It's not a prime number.")
         
-#Write your code above this line 👆
     
-#Do NOT change any of the code below👇
 n = int(input("Check this number: "))
 prime_checker(number=n)
